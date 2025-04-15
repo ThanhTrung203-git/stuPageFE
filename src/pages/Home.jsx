fa1548c0fad027aca703472585930753bec668af
ThanhTrung203-git <nhoxtrungcuti@gmail.com>
2025-04-15 07:59:29 +0700

    deloy4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import ContentPage from "../components/content/Content";
 import ContentRightPage from "../components/content/contentRight";
 import Header from "../components/Header/Header";
+import Slideshow from '../components/UI/SlideShow';
 import '../App.css'
+import logo1 from '../assets/tuyensinh2025.jpg'
+import logo2 from '../assets/tuyensinh2025-2.png'
 
 const Home = () => {
+  const imageList = [
+      logo1,logo2
+    ];
   return (
     <>
+      <Slideshow images={imageList} interval={3000}/>
       <div className="content-container">
         <div className="left-column">
             <ContentPage/>
