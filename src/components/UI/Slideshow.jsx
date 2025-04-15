c096d39f4efb2810dfb8cd74650fa8ae9a0acd64
ThanhTrung203-git <nhoxtrungcuti@gmail.com>
2025-04-15 08:02:28 +0700

    deloy5

--- src/components/UI/Slideshow.jsx
+++ src/components/UI/Slideshow.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Slideshow.css';
 
-const SlideShow = ({ images, interval = 3000 }) => {
+const Slideshow = ({ images, interval = 3000 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -28,4 +28,4 @@ const SlideShow = ({ images, interval = 3000 }) => {
   );
 };
 
-export default SlideShow;
+export default Slideshow;
